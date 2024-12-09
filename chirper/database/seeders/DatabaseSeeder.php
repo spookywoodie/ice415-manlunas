bcbbdddee33ad483187e2104dde774f109724486
rea may <manlunasrea293@gmail.com>
2024-12-09 10:36:51 +0800

    changes
    Edit, delete and notify

--- chirper/database/seeders/DatabaseSeeder.php
+++ chirper/database/seeders/DatabaseSeeder.php
@@ -3,21 +3,18 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UserSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'password' => bcrypt('password'),
         ]);
     }
+}
+
 }
